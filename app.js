f6f4dcc6af3c08295b488b9d48b78107ab8ffab3
Sam11dot <samruddhichaudhari185@gmail.com>
2026-02-07 01:33:21 +0530

    updated

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const connectDB = require("./config/db"); // 👈 important
+const connectDB = require("./config/db");
+const path = require("path");
 
 dotenv.config();
+
 
 let isConnected = false;
 
@@ -11,27 +13,36 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Root route (fixes 404)
-app.get("/", (req, res) => {
-  res.send("Backend is running 🚀");
-});
-
-// DB connection
+// Middleware to ensure DB connection
 app.use(async (req, res, next) => {
   if (!isConnected) {
     try {
       await connectDB();
       isConnected = true;
+      console.log("MongoDB connected");
     } catch (err) {
+      console.error("DB Connection Error:", err.message);
       return res.status(500).json({ error: "Database connection failed" });
     }
   }
   next();
 });
 
-// Routes
+
+// 🔥 Connect DB immediately
+// connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api", require("./routes/downloadRoutes"));
 app.use("/api", require("./routes/pdfRoutes"));
 
-module.exports = app;
+// const PORT = process.env.PORT || 5016;
+
+// app.listen(PORT, () => {
+//   console.log(`🚀 Server running on port ${PORT}`);
+// });
+module.exports = app ;
