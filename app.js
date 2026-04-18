a36437ce90712966cba4fd4f16a41f1c660f2ba9
Sam11dot <samruddhichaudhari185@gmail.com>
2026-04-18 14:43:24 +0530

    updated env app.js0

--- app.js
+++ app.js
@@ -6,31 +6,10 @@ const path = require("path");
 
 dotenv.config();
 
-
-let isConnected = false;
-
 const app = express();
-app.use(cors());
-app.use(express.json());
-
-// Middleware to ensure DB connection
-app.use(async (req, res, next) => {
-  if (!isConnected) {
-    try {
-      await connectDB();
-      isConnected = true;
-      console.log("MongoDB connected");
-    } catch (err) {
-      console.error("DB Connection Error:", err.message);
-      return res.status(500).json({ error: "Database connection failed" });
-    }
-  }
-  next();
-});
-
 
 // 🔥 Connect DB immediately
-// connectDB();
+connectDB();
 
 app.use(cors());
 app.use(express.json());
@@ -40,9 +19,8 @@ app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api", require("./routes/downloadRoutes"));
 app.use("/api", require("./routes/pdfRoutes"));
 
-// const PORT = process.env.PORT || 5016;
+const PORT = process.env.PORT || 5016;
 
-// app.listen(PORT, () => {
-//   console.log(`🚀 Server running on port ${PORT}`);
-// });
-module.exports = app ;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
